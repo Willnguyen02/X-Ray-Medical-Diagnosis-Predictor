18e9a7d1623a6110b74400b117d3a4ca814523dd
William Nguyen <william3709@gmail.com>
2025-05-31 15:43:17 -0700

    updates to data preprocessing phase

--- scripts/extract_imgs.py
+++ scripts/extract_imgs.py
@@ -2,7 +2,59 @@
 
 import pandas as pd
 import kagglehub
+import shutil
 import os 
+import warnings
+warnings.filterwarnings("ignore")
 
 
 path = kagglehub.dataset_download("nih-chest-xrays/data")
+
+# locates the parent folder of each image and adds it to the dataset as a column
+def find_img_folder(dataset):
+    targeted_imgs = dataset["image_index"]
+
+    #  build a map of image filename 
+    image_to_folder = {}
+
+    for folder in os.listdir(path):
+        if folder.startswith("images"):
+            folder_path = os.path.join(path, folder, "images")
+            try:
+                for img_file in os.listdir(folder_path):
+                    image_to_folder[img_file] = folder  
+            except FileNotFoundError:
+                continue  
+
+    # use the map to find which folder each targeted image is in
+    parent_folders = []
+    for img in targeted_imgs:
+        folder = image_to_folder.get(img)
+        if folder:
+            parent_folders.append(folder)
+        else:
+            print(f"Not found: {img}")
+
+    # add extracted parent folder to dataframe as a new column
+    parent_folders_df = pd.DataFrame({
+        "image_index": targeted_imgs,
+        "src_folder": parent_folders
+        })
+    dataset = pd.merge(dataset, parent_folders_df, on="image_index")
+
+    return dataset
+
+
+# copies images to a specified folder
+def move_imgs(dataset, destination):
+
+    # locates the source path for all images in the dataset
+    for row in range(dataset.shape[0]):
+        source_path = os.path.join(path, dataset.iloc[row]["src_folder"], "images", dataset.iloc[row]["image_index"])
+        dest_path = os.path.join(destination, dataset.iloc[row]["image_index"])
+
+        # skips if image already exists in the specified destination
+        if os.path.exists(dest_path):
+            return
+        else:
+            shutil.copy2(source_path, dest_path)
